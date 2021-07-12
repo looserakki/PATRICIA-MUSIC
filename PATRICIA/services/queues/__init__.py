f68fa2d3711519975a0c0873ae41668b92ebbede
TEAM-PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-07-12 10:52:46 +0530

    Update __init__.py

--- PATRICIA/services/queues/__init__.py
+++ PATRICIA/services/queues/__init__.py
@@ -1,1 +1,7 @@
+from PATRICIA.services.queues.queues import clear 
+from PATRICIA.services.queues.queues import get
+from PATRICIA.services.queues.queues import is_empty
+from PATRICIA.services.queues.queues import put
+from PATRICIA.services.queues.queues import task_done
 
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
